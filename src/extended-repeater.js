ff7fc213c3cf6e0d912b99670cfc11d882d8ecd4
Andrey Ushakov <ushakov.andrey257686@gmail.com>
2023-12-22 17:17:20 +0500

    feat: done 14/21

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,8 +15,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
+function repeater(str, options) {
+  let result = '';
+  let resultAddition = '';
+  const funcRepeatTimes = !options.repeatTimes ? 1 : options.repeatTimes;
+  const funcSeparator = !options.separator ? "+" : options.separator;
+  const funcAddition = options.addition === undefined ? "" : String(options.addition);
+  const funcAdditionRepeatTimes = !options.additionRepeatTimes ? 1 : options.additionRepeatTimes;
+  const funcAdditionSeparator = !options.additionSeparator ? "|" : options.additionSeparator;
+  if (funcAdditionRepeatTimes === 1) {
+    resultAddition = funcAddition;
+  } else {
+    resultAddition = (funcAddition + funcAdditionSeparator).repeat(funcAdditionRepeatTimes);
+    resultAddition = resultAddition.slice(0, -(funcAdditionSeparator.length));
+  }
+  result = (str + resultAddition + funcSeparator).repeat(funcRepeatTimes);
+  result = result.slice(0, -(funcSeparator.length));
+  return result
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
 
--- src/sum-digits.js
+++ src/sum-digits.js
@@ -12,8 +12,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
+function getSumOfDigits(n) {
+  let arrN = n.toString().split('');
+  let result = 0;
+  arrN.forEach((digit) => {
+    result = result + Number(digit);
+  });
+  if (result >= 10) {
+    return getSumOfDigits(result);
+  } else {
+    return result;
+  }
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
 
