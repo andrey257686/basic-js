35580a8488a273c17ae2f834e672f4c955a26f29
Andrey Ushakov <ushakov.andrey257686@gmail.com>
2023-12-24 15:29:52 +0500

    feat: done 21/21

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,8 +13,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
+function transform(arr) {
+  if (!Array.isArray(arr)){
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+  if (arr.length === 0) {
+    return [];
+  }
+  console.debug(arr);
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === '--double-next' && i !== arr.length - 1) {
+      result.push(arr[i+1]);
+    }
+    if (arr[i] === '--double-prev' && arr[i-2] !== '--discard-next' && i !== 0) {
+      result.push(arr[i-1]);
+    }
+    if (arr[i] === '--discard-next' && i !== arr.length - 1) {
+      i = i + 2;
+    }
+    if (arr[i] === '--discard-prev' && arr[i-2] !== '--discard-next' && i !== 0) {
+      result.pop()
+    }
+    if (arr[i] !== '--double-next' && arr[i] !== '--double-prev' &&  arr[i] !== '--discard-next' &&  arr[i] !== '--discard-prev') {
+      result.push(arr[i]);
+    }
+  }
+  return result;
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
 
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,13 +19,77 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
+
+// alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
+  constructor (type = true) {
+    this.type = type;
+  }
+  encrypt(msg, key) {
+    if (!msg || !key) {
+      throw new Error("Incorrect arguments!");
+    }
+    let msgArr = msg.toUpperCase().split('');
+    let resultArr = [];
+    let result = '';
+    let msgKey = [];
+    key = key.toUpperCase();
+    let j = 0;
+    let offset = 0;
+    for (let i = 0; i < msgArr.length; i++) {
+      msgKey.push(key[i%(key.length)]);
+      j = i  - offset;
+      if (msgArr[i].charCodeAt(0) >= 65 && msgArr[i].charCodeAt(0) <= 90) {
+        resultArr.push(String.fromCharCode(65 + (msgArr[i].charCodeAt(0) + msgKey[j].charCodeAt(0)) % 26));
+      } else {
+        resultArr.push(msgArr[i]);
+        offset = offset + 1;
+      }
+    }
+    console.log(resultArr.join(''))
+    if (this.type) {
+      return resultArr.join('')
+    }
+    else {
+      return resultArr.reverse().join('')
+    }
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
+  decrypt(msg, key) {
+    if (!msg || !key) {
+      throw new Error("Incorrect arguments!");
+    }
+    let msgArr = msg.toUpperCase().split('');
+    let resultArr = [];
+    let result = '';
+    let msgKey = [];
+    key = key.toUpperCase();
+    let j = 0;
+    let offset = 0;
+    for (let i = 0; i < msgArr.length; i++) {
+      msgKey.push(key[i%(key.length)]);
+      j = i  - offset;
+      if (msgArr[i].charCodeAt(0) >= 65 && msgArr[i].charCodeAt(0) <= 90) {
+        let id = 0;
+        if (((msgArr[i].charCodeAt(0) - msgKey[j].charCodeAt(0)) % 26) < 0) {
+          id = ((msgArr[i].charCodeAt(0) - msgKey[j].charCodeAt(0)) % 26) + 26;
+          resultArr.push(String.fromCharCode(65 + id));
+        } else {
+          resultArr.push(String.fromCharCode(65 + (msgArr[i].charCodeAt(0) - msgKey[j].charCodeAt(0)) % 26));
+        }
+      } else {
+        resultArr.push(msgArr[i]);
+        offset = offset + 1;
+      }
+    }
+    if (this.type) {
+      return resultArr.join('')
+    }
+    else {
+      return resultArr.reverse().join('')
+    }
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 }
